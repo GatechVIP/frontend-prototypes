4b3dffd3e1d3bbd62586be7b395a857b114e26af
Alvin Yan <ayan6@gatech.edu>
2013-04-10 18:21:16 -0400

    Finished annotation editing changes

--- js/annotation.js
+++ js/annotation.js
@@ -4,21 +4,23 @@ function annotate(e) {
     console.log(inText);
     if ($('div.popup').length == 0 && t.toString().length > 0) {
         inText = true;
-        $("body").append('<div class="popup" style="position:absolute; z-index: 500; background-color:#FFFFFF; border-style:solid; top:' + e.pageY + 'px; left:' + e.pageX + 'px;"><form name="annotation"><textarea rows=5 cols=25 id="annotate"></textarea><input type=button value="Save" onclick="saveComment(' + e.pageY + ')"></form></div>');
+        $("body").append('<div class="popup" style="position:absolute; z-index: 5000; background-color:#FFFFFF; border-style:solid; top:' + e.pageY + 'px; left:' + e.pageX + 'px;"><form name="annotation"><textarea rows=5 cols=25 id="annotate"></textarea><input type=button value="Save" onclick="saveComment(' + e.pageY + ')"></form></div>');
     }
 }
 
 function toggleEdit(object) {
+    console.log("toggleEdit");
     var pageY = object.getBoundingClientRect().top;
     var pageX = object.getBoundingClientRect().left;
-    $("body").append('<div class="popup" style="position:absolute; z-index: 500; background-color:#FFFFFF; border-style:solid; top:' + pageY + 'px; left:' + pageX + 'px;"><form name="annotation"><textarea rows=5 cols=25 id="annotate">'+ object.innerHTML +'</textarea><input type=button value="Save" onclick="saveComment(' + pageY + ')"></form></div>');
+    $("body").append('<div class="popup" style="position:absolute; z-index: 5000; background-color:#FFFFFF; border-style:solid; top:' + pageY + 'px; left:' + pageX + 'px;"><form name="annotation"><textarea rows=5 cols=25 id="annotate">'+ object.innerHTML +'</textarea><input type=button value="Save" onclick="saveComment(' + pageY + ')"></form></div>');
     object.parentNode.removeChild(object);
 }
 
 function toggleEditRight(object, pageY) {
+    console.log("toggleEditRight");
     //var pageY = object.getBoundingClientRect().top;
     var pageX = object.getBoundingClientRect().left;
-    $("body").append('<div class="popup" style="position:absolute; z-index: 500; background-color:#FFFFFF; border-style:solid; top:' + pageY + 'px; left:' + pageX + 'px;"><form name="annotation"><textarea rows=5 cols=25 id="annotate">' + object.innerHTML + '</textarea><input type=button value="Save" onclick="updateCommentRight(' + pageY + ')"></form></div>');
+    $("body").append('<div class="popup" style="position:absolute; z-index: 5000; background-color:#FFFFFF; border-style:solid; top:' + pageY + 'px; left:' + pageX + 'px;"><form name="annotation"><textarea rows=5 cols=25 id="annotate">' + object.innerHTML + '</textarea><input type=button value="Save" onclick="updateCommentRight(' + pageY + ')"></form></div>');
     object.parentNode.removeChild(object);
 }
 
@@ -26,7 +28,7 @@ function customAnnotate(e) {
     console.log("customAnnotate");
     if ($('div.popup').length == 0) {
         inText = true;
-        $("body").append('<div class="popup" style="position:absolute; background-color:#FFFFFF; border-style:solid; top:' + (e.pageY + 20) + 'px; left:' + (e.pageX - 50) + 'px;"><form name="annotation"><textarea rows=5 cols=25 id="annotate"></textarea><input type=button value="Save" onclick="saveCommentRight(' + e.pageY + ')"></form></div>');
+        $("body").append('<div class="popup" style="position:absolute; z-index:5000; background-color:#FFFFFF; border-style:solid; top:' + (e.pageY + 20) + 'px; left:' + (e.pageX - 50) + 'px;"><form name="annotation"><textarea rows=5 cols=25 id="annotate"></textarea><input type=button value="Save" onclick="saveCommentRight(' + e.pageY + ')"></form></div>');
     }
 }
 
@@ -39,14 +41,14 @@ function saveComment(pageY) {
 function updateCommentRight(pageY) {
     rightTopPadding -= 50;
     console.log("updateCommentRight");
-    $("body").append('<div class="margin" onclick="toggleEditRight(this)" style="z-index:1000; position:absolute; top:' + (pageY + rightTopPadding) + 'px; right:0px">' + $("#annotate").val() + '</div>');
+    $("body").append('<div class="margin" onclick="toggleEditRight(this,' + pageY + ')" style="z-index:1000; position:absolute; top:' + (pageY) + 'px; right:0px">' + $("#annotate").val() + '</div>');
     $('div.popup').remove();
     rightTopPadding += 50;
 }
 
 function saveCommentRight(pageY) {
-    console.log("saveCommentRight");
-    $("body").append('<div class="margin" onclick="toggleEditRight(this,'+pageY+')" style="z-index:1000; position:absolute; top:' + (pageY + rightTopPadding) + 'px; right:0px">' + $("#annotate").val() + '</div>');
+    console.log("saveCommentRight" + pageY);
+    $("body").append('<div class="margin" onclick="toggleEditRight(this,'+rightTopPadding+')" style="z-index:1000; position:absolute; top:' + (rightTopPadding) + 'px; right:0px">' + $("#annotate").val() + '</div>');
     $('div.popup').remove();
     rightTopPadding += 50;
 }
